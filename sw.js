e4325c27afb50f4c3ec7b61edec197c1251d7a13
belalbelal724-stack <belal.belal724@gmail.com>
2026-07-04 16:18:06 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "qatar-deals-v18";
+const CACHE = "qatar-deals-v19";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
